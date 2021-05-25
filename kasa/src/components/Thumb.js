1a65d08830886cba28a1d84cb7bce8d1a6a56381
Exvigilare Gemini <dubois.adelin@gmail.com>
2021-05-25 16:31:36 +0200

    Thumb component -> layout

--- kasa/src/components/Thumb.js
+++ kasa/src/components/Thumb.js
@@ -7,6 +7,7 @@ class Thumb extends React.Component {
                 return (
                         <div className="thumb" key={this.props.id} >
                             <Link to="Logement">
+                            <div className="thumbTransparentEffect"></div>
                             <img className="thumbImage" src={this.props.cover} alt="Présentation du lieu"></img>
                             <p className="thumbTitle">{this.props.title}</p>
                             </Link>
