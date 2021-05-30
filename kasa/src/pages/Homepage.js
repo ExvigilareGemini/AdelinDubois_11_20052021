0c9999b1a3e056d99548574203a73e36c15ad30f
Exvigilare Gemini <dubois.adelin@gmail.com>
2021-05-30 20:41:13 +0200

    homepage responsive

--- kasa/src/pages/Homepage.js
+++ kasa/src/pages/Homepage.js
@@ -9,9 +9,11 @@ class Homepage extends React.Component {
     render() {
         return  <div className="main">
                     <Banner image={"https://photographersbreakthrough.com/wp-content/uploads/2020/05/eric-muhr-P_XxsdVgtpQ-unsplash.jpg"} text="Chez vous, partout et ailleurs"/>
-                    {Datas.map((data) => {
-                        return <Thumb key={data.id} id={data.id} cover={data.cover} title={data.title} />
-                    })}
+                    <div className="thumbContainer">
+                        {Datas.map((data) => {
+                            return <Thumb key={data.id} id={data.id} cover={data.cover} title={data.title} />
+                        })}
+                    </div>
                 </div>
             
     }
